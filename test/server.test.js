332920247240d95b8ae9e65c4c550af014c0c034
Wesley Costa <wesferrcosta@gmail.com>
2021-01-31 20:03:47 -0300

    Deve responder na raiz

--- test/server.test.js
+++
@@ -1,7 +0,0 @@
-const supertest = require('supertest');
-const request = supertest('http://localhost:3001');
-
-// eslint-disable-next-line arrow-body-style
-test('Deve responder na porta 3001', () => {
-  return request.get('/').then((res) => expect(res.status).toBe(200));
-});
