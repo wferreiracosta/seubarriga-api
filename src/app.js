12b2f0ddf7d5438e00beccbeca9f0a877664c3b4
Wesley Costa <wesferrcosta@gmail.com>
2021-02-04 15:35:58 -0300

    Organizando arquivos

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 const app = require('express')();
+const consign = require('consign');
+
+consign({ cwd: 'src', verboso: false })
+  .include('./config/middlewares.js')
+  .into(app);
 
 app.get('/', (req, res) => {
   res.status(200).send();
@@ -9,6 +14,10 @@ app.get('/users', (req, res) => {
     { name: 'John Doe', mail: 'john@mail.com' },
   ];
   res.status(200).json(users);
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).json(req.body);
 });
 
 module.exports = app;
