21e1b12275f45b6f3b28a3e6b534e4c266d9f086
Wesley Costa <wesferrcosta@gmail.com>
2021-02-05 00:18:28 -0300

    Add: Catch

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -42,5 +42,6 @@ test('Não deve inserir usuário sem senha', async (done) => {
       expect(res.status).toBe(400);
       expect(res.body.error).toBe('Senha é um atributo obrigatório');
       done();
-    });
+    })
+    .catch((err) => done.fail(err));
 });
