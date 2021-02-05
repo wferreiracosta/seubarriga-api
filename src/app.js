d018c0342b2e7c148469bee852a07d8748035229
Wesley Costa <wesferrcosta@gmail.com>
2021-02-04 22:28:40 -0300

    Log de consultas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
+const knexlogger = require('knex-logger');
 const knexfile = require('../knexfile');
 
 app.db = knex(knexfile.test);
+
+app.use(knexlogger(app.db));
 
 consign({ cwd: 'src', verboso: false })
   .include('./config/middlewares.js')
