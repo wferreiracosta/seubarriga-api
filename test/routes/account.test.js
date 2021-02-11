d82fc5c12a0561920b777262ed09e9251c2f54ef
Wesley Costa <wesferrcosta@gmail.com>
2021-02-11 18:08:42 -0300

    Implementado busca de todas as contas

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -3,10 +3,10 @@ const app = require('../../src/app');
 
 const MAIN_ROUTE = '/accounts';
 const email = `${Date.now()}@mail.com`;
+const newUser = { name: 'Walter Mitty', email, passwd: '123456' };
 let user;
 
 beforeAll(async () => {
-  const newUser = { name: 'Walter Mitty', email, passwd: '123456' };
   await app.services.user.save(newUser);
   return app.services.user.findAll(newUser)
     .then((returnedUser) => {
@@ -20,5 +20,15 @@ test('Deve inserir uma conta com sucesso', () => {
     .then((result) => {
       expect(result.status).toBe(201);
       expect(result.body.name).toBe('Acc #1');
+    });
+});
+
+test('Deve listar todas contas', () => {
+  return app.db('accounts')
+    .insert({ name: 'Acc #1', user_id: user.id })
+    .then(() => request(app).get(MAIN_ROUTE))
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBeGreaterThan(0);
     });
 });
